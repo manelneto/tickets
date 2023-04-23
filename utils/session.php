a72aeb99558d1b1b66f029bc85108f30a9b3dbe3
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-23 20:40:35 +0100

    added session messages (unused for now) and getters/setters to session attributes

--- utils/session.php
+++ utils/session.php
@@ -1,7 +1,14 @@
 <?php
+    declare(strict_types = 1);
+
     class Session {
+        private array $messages;
+
         public function __construct() {
             session_start();
+
+            $this->messages = isset($_SESSION['messages']) ? $_SESSION['messages'] : array();
+            unset($_SESSION['messages']);
         }
 
         public function isLoggedIn() : bool {
@@ -9,7 +16,39 @@
         }
 
         public function logout() {
-            session_destoy();
+            session_destroy();
+        }
+
+        public function getId() : ?int {
+            return $_SESSION['id'];
+        }
+
+        public function getUsername() : ?string {
+            return $_SESSION['username'];
+        }
+
+        public function getName() : ?string {
+            return $_SESSION['name'];
+        }
+
+        public function setId(int $id) {
+            $_SESSION['id'] = $id;
+        }
+
+        public function setUsername(string $username) {
+            $_SESSION['username'] = $username;
+        }
+
+        public function setName(string $name) {
+            $_SESSION['name'] = $name;
+        }
+
+        public function addMessage(string $type, string $text) {
+            $_SESSION['messages'][] = array('type' => $type, 'text' => $text);
+        }
+
+        public function getMessages() {
+            return $this->messages;
         }
     }
 ?>
