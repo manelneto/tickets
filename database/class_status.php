a4360f6a2b5bd3db6f4cc69780dca98060933f03
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-26 17:44:18 +0100

    added getStatus() and getStatuses()

--- database/class_status.php
+++ database/class_status.php
@@ -21,5 +21,45 @@
         public function setName(string $name) {
             $this->name = $name;
         }
+
+        public static function getStatuses(PDO $db) : ?array {
+            $stmt = $db->prepare('
+                SELECT idStatus, name
+                FROM Status
+            ');
+
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+
+            if (!$result) return null;
+
+            $statuses = array();
+
+            foreach ($result as $row)
+                $statuses[] = new Status(
+                    (int) $row['idStatus'],
+                    $row['name']
+                );
+
+            return $statuses;
+        }
+        
+        public static function getStatus(PDO $db, int $id) : ?Status {
+            $stmt = $db->prepare('
+                SELECT idStatus, name
+                FROM Status
+                Where idStatus = ?
+            ');
+
+            $stmt->execute(array($id));
+            $status = $stmt->fetch();
+
+            if (!$status) return null;
+
+            return new Status(
+                (int) $status['idStatus'],
+                $status['name']
+            );
+        }
     }
 ?>
