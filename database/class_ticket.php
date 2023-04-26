a6ecd242cfba58230837fe1df43ad8559cf5d6c8
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-26 16:06:02 +0100

    added missing visibility keyword

---
+++ database/class_ticket.php
@@ -0,0 +1,169 @@
+<?php
+    declare(strict_types = 1);
+
+    require_once(__DIR__ . '/../database/class_user.php');
+
+    class Ticket {
+        private int $id;
+        private User $client;
+        private string $title;
+        private string $content;
+        private string $dateOpened;
+        private string $dateDue;
+        private string $dateClosed;
+        private User $agent;
+        private Department $department;
+        private Priority $prioriy
+
+        public function __construct(int $id, string $firstName, string $lastName, string $username, string $email) {
+            $this->id = $id;
+            $this->firstName = $firstName;
+            $this->lastName = $lastName;
+            $this->username = $username;
+            $this->email = $email;
+        }
+
+        public function getId() : int {
+            return $this->id;
+        }
+
+        public function getFirstName() : string {
+            return $this->firstName;
+        }
+
+        public function getLastName() : string {
+            return $this->lastName;
+        }
+
+        public function getUsername() : string {
+            return $this->username;
+        }
+
+        public function getEmail() : string {
+            return $this->email;
+        }
+
+        public function getName() : string {
+            return $this->firstName . ' ' . $this->lastName;
+        }
+
+        public function setFirstName(string $firstName) {
+            $this->firstName = $firstName;
+        }
+
+        public function setLastName(string $lastName) {
+            $this->lastName = $lastName;
+        }
+
+        public function setUsername(string $username) {
+            $this->username = $username;
+        }
+
+        public function setEmail(string $email) {
+            $this->email = $email;
+        }
+
+        public function isAgent(PDO $db) : bool {
+            $stmt = $db->prepare('
+                SELECT *
+                FROM Agent
+                WHERE idAgent = ?
+            ');
+
+            $stmt->execute(array($this->id));
+
+            return (bool) $stmt->fetch();
+        }
+
+        public function isAdmin(PDO $db) : bool {
+            $stmt = $db->prepare('
+                SELECT *
+                FROM Admin
+                WHERE idAdmin = ?
+            ');
+
+            $stmt->execute(array($this->id));
+
+            return (bool) $stmt->fetch();
+        }
+
+        function update(PDO $db, string $firstName, string $lastName, string $username, string $email) : bool {
+            $stmt = $db->prepare('
+                UPDATE User
+                SET firstName = ?, lastName = ?, username = ?, email = ?
+                WHERE idUser = ?
+            ');
+
+            try {
+                $stmt->execute(array($firstName, $lastName, $username, $email, $this->id));
+            } catch (PDOException $e) {
+                return false;
+            }
+            
+            $this->firstName = $firstName;
+            $this->lastName = $lastName;
+            $this->username = $username;
+            $this->email = $email;
+            return true;
+        }
+
+        public static function getUser(PDO $db, int $id) : ?User {
+            $stmt = $db->prepare('
+                SELECT idUser, firstName, lastName, username, email
+                FROM User
+                Where idUser = ?
+            ');
+
+            $stmt->execute(array($id));
+            $user = $stmt->fetch();
+
+            if (!$user) return null;
+
+            return new User(
+                (int) $user['idUser'],
+                $user['firstName'],
+                $user['lastName'],
+                $user['username'],
+                $user['email']
+            );
+        }
+
+        public static function loginUser(PDO $db, string $username, string $password) : ?User {
+            $stmt = $db->prepare('
+                SELECT idUser, firstName, lastName, username, email, password
+                FROM User
+                WHERE lower(username) = ?
+            ');
+
+            $stmt->execute(array(strtolower($username)));
+            $user = $stmt->fetch();
+
+            if ($user && password_verify($password, $user['password'])) {
+                return new User(
+                    (int) $user['idUser'],
+                    $user['firstName'],
+                    $user['lastName'],
+                    $user['username'],
+                    $user['email']
+                );
+            } else return null;
+        }
+
+        public static function registerUser(PDO $db, string $firstName, string $lastName, string $username, string $email, string $password) : ?User {
+            $options = ['cost' => 12];
+                        
+            $stmt = $db->prepare('
+                INSERT INTO User (firstName, lastName, username, email, password)
+                VALUES (?, ?, ?, ?, ?)
+            ');
+
+            try {
+                $stmt->execute(array($firstName, $lastName, $username, $email, password_hash($password, PASSWORD_DEFAULT, $options)));
+            } catch (PDOException $e) {
+                return null;
+            }
+
+            return User::loginUser($db, $username, $password);
+        }
+    }
+?>
