cc41edd4d40185a8846491e26be83af377527f75
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-26 16:08:48 +0100

    added class tag

---
+++ database/class_tag.php
@@ -0,0 +1,25 @@
+<?php
+    declare(strict_types = 1);
+
+    class Tag {
+        private int $id;
+        private string $name;
+
+        public function __construct(int $id, string $name) {
+            $this->id = $id;
+            $this->name = $name;
+        }
+
+        public function getId() : int {
+            return $this->id;
+        }
+
+        public function getName() : string {
+            return $this->name;
+        }
+
+        public function setName(string $name) {
+            $this->name = $name;
+        }
+    }
+?>
