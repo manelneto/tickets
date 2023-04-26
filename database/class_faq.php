933e4dc79ae8dfd70178331633dadbcf17c253c6
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-26 17:43:47 +0100

    added getFAQ()

--- database/class_faq.php
+++ database/class_faq.php
@@ -46,9 +46,32 @@
             $faqs = array();
 
             foreach ($result as $row)
-                $faqs[] = new FAQ((int) $row['idFAQ'], $row['question'], $row['answer']);
+                $faqs[] = new FAQ(
+                    (int) $row['idFAQ'],
+                    $row['question'],
+                    $row['answer']
+                );
 
             return $faqs;
+        }
+
+        public static function getFAQ(PDO $db, int $id) : ?FAQ {
+            $stmt = $db->prepare('
+                SELECT idFAQ, question, answer
+                FROM FAQ
+                Where idFAQ = ?
+            ');
+
+            $stmt->execute(array($id));
+            $faq = $stmt->fetch();
+
+            if (!$faq) return null;
+
+            return new FAQ(
+                (int) $faq['idFAQ'],
+                $faq['question'],
+                $faq['answer']
+            );
         }
     }
 ?>
