5625eae475b9649136ba1c658d4b12d65865a351
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-26 17:43:35 +0100

    added getDepartment and getDepartments()

--- database/class_department.php
+++ database/class_department.php
@@ -21,5 +21,45 @@
         public function setName(string $name) {
             $this->name = $name;
         }
+
+        public static function getDepartments(PDO $db) : ?array {
+            $stmt = $db->prepare('
+                SELECT idDepartment, name
+                FROM Department
+            ');
+
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+
+            if (!$result) return null;
+
+            $departments = array();
+
+            foreach ($result as $row)
+                $departments[] = new Department(
+                    (int) $row['idDepartment'],
+                    $row['name']
+                );
+
+            return $departments;
+        }
+
+        public static function getDepartment(PDO $db, int $id) : ?Department {
+            $stmt = $db->prepare('
+                SELECT idDepartment, name
+                FROM Department
+                Where idDepartment = ?
+            ');
+
+            $stmt->execute(array($id));
+            $department = $stmt->fetch();
+
+            if (!$department) return null;
+
+            return new Department(
+                (int) $department['idDepartment'],
+                $department['name']
+            );
+        }
     }
 ?>
