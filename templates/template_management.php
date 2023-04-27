1c3384ffb2097dbbbcc31753eb181900680cb4f7
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-27 14:13:42 +0100

    added form lists and actions

--- templates/template_management.php
+++ templates/template_management.php
@@ -2,16 +2,26 @@
     declare(strict_types = 1);
 ?>
 
-<?php function drawManagemnt() { ?>
+<?php function drawManagemnt(array $clients, array $departments, array $agents) { ?>
     <section id="management">
         <h2>Management</h2>
-        <form class="upgrade">
+        <form action="../actions/action_upgrade_client.php" method="post" class="upgrade">
             <h3>Upgrade a client</h3>
             <label for="client">Select a client</label>
-            <!-- LISTA COM HIPÓTESE DE ESCREVER ???? -->
+            <select id="client" name="client">
+                <?php for ($clients as $client) { ?>
+                <option value="<?=$client->getId()?>"><?=$client->getUsername()?></option>
+                <?php } ?>
+            </select>
+            <label>To agent
+                <input type="radio" name="agent" value="agent">
+            </label>
+            <label>To admin
+                <input type="radio" name="admin" value="admin">
+            </label>
             <button type="submit" id="add">Upgrade</button>
         </form>
-        <form class="entity">
+        <form action="../actions/action_add_entity.php" method="post"class="entity">
             <h3>Add a new entity</h3>
             <label for="entity">Select a entity</label>
             <select id="entity" name="entity">
@@ -24,16 +34,20 @@
             <input type="text" id="name" name="name">
             <button type="submit" id="add">Add</button>
         </form>
-        <form class="assign">
+        <form action="../actions/action_assign_agent.php" method="post" class="assign">
             <h3>Assign agent to department</h3>
             <label for="departmnt">Select a department</label>
             <select id="department" name="department">
                 <?php for ($departments as $department) { ?>
-                <option value="<?=$department->getName()?>"><?=$department->getName()?></option>
+                <option value="<?=$department->getId()?>"><?=$department->getName()?></option>
                 <?php } ?>
             </select>
-            <label for="agent">Select a agent</label>
-            <!-- LISTA COM HIPÓTESE DE ESCREVER ???? -->
+            <label for="agent">Select an agent</label>
+            <select id="agent" name="agent">
+                <?php for ($agents as $agent) { ?>
+                <option value="<?=$agent->getId()?>"><?=$agent->getUsername()?></option>
+                <?php } ?>
+            </select>
             <button type="submit" id="add">Assign</button>
         </form>
     </section>
