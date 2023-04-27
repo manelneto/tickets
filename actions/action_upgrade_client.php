08d68fa57fb72adede3c025dae4d6afad24bc61f
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-27 14:42:56 +0100

    added action to upgrade client

---
+++ actions/action_upgrade_client.php
@@ -0,0 +1,26 @@
+<?php
+    declare(strict_types = 1);
+
+    require_once(__DIR__ . '/../utils/session.php');
+    $session = new Session();
+
+    if (!$session->isAdmin()) {
+        header('Location: ../pages/index.php');
+        die();
+    }
+
+    require_once(__DIR__ . '/../database/connection.php');
+    $db = getDatabaseConnection();
+
+    require_once(__DIR__ . '/../database/class_user.php');
+    $client = User::getUser($db, $_POST['client']);
+
+    if ($client && $_POST['role'] === 'agent') {
+        $client->upgradeToAgent();
+        header('Location: ../pages/dashboard.php');
+    } else if ($client && $_POST['role'] === 'admin') {
+        $client->upgradeToAdmin();
+        header('Location: ../pages/dashboard.php');
+    } else
+        header('Location: ../pages/management.php');
+?>
