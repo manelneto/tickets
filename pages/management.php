9fd9eef793179abcb0d8079b154f5dddf451984e
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-27 14:17:26 +0100

    added arrays with data to management.php

--- pages/management.php
+++ pages/management.php
@@ -4,15 +4,25 @@
     require_once(__DIR__ . '/../utils/session.php');
     $session = new Session();
 
-    if (!$session->isLoggedIn()) {
+    if (!$session->isAdmin()) {
         header('Location: ../pages/index.php');
         die();
     }
+
+    require_once(__DIR__ . '/../database/connection.php');
+    $db = getDatabaseConnection();
+
+    require_once(__DIR__ . '/../database/class_user.php');
+    $clients = User::getClients($db);
+    $agents = User::getAgents($db);
+
+    require_once(__DIR__ . '/../database/class_department.php');
+    $departments = Department::getDepartments($db);
 
     require_once(__DIR__ . '/../templates/template_common.php');
     require_once(__DIR__ . '/../templates/template_management.php');
 
     drawHeader($session);
-    drawManagement();
+    drawManagement($clients, $departments, $agents);
     drawFooter();
 ?>
