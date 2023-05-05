dc240791633491dec1a2e02290af61bcc20fd214
Matilde Simões <up202108782@up.pt>
2023-05-06 00:06:28 +0100

    update dashboard.php

--- templates/template_dashboard.php
+++ templates/template_dashboard.php
@@ -3,20 +3,22 @@
 ?>
 
 <?php function drawDashboard(int $opened, int $assigned, int $closed, int $overdue) { ?>
-    <section class="opened">
-        <h2>Opened</h2>
-        <p><?=$opened?></p>
-    </section>
-    <section class="assigned">
-        <h2>Assigned</h2>
-        <p><?=$assigned?></p>
-    </section>
-    <section class="closed">
-        <h2>Closed</h2>
-        <p><?=$closed?></p>
-    </section>
-    <section class="overdue">
-        <h2>Overdue</h2>
-        <p><?=$overdue?></p>
-    </section>
+    <div class = "dashboard-tickets"> <!--adicionei sem autorização do manel mas e essencial para o css-->
+        <section class="opened">
+            <h2> Opened </h2>
+            <p>You have <?=$opened?> tickets opened!</p>
+        </section>
+        <section class="assigned">
+            <h2>Assigned</h2>
+            <p>You have <?=$assigned?> tickets assigned!</p>
+        </section>
+        <section class="closed">
+            <h2> Closed </h2>
+            <p>You have <?=$closed?> tickets closed!</p>
+        </section>
+        <section class="overdue">
+            <h2>Overdue</h2>
+            <p>You have <?=$overdue?> tickets overdue!</p>
+        </section>
+    </div>
 <?php } ?>
