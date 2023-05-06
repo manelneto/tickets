867383a7cebffdb999246b6001568d12e9cff2ce
Matilde Simões <up202108782@up.pt>
2023-05-06 11:50:45 +0100

    update login.php

--- templates/template_login.php
+++ templates/template_login.php
@@ -3,16 +3,17 @@
 ?>
 
 <?php function drawLogin() { ?>
-    <h1><a href="../pages/index.php">Tickets Management</a></h1>
     <section id="login">
         <h2>Login</h2>
         <form action="../actions/action_login.php" method="post" class="login">
             <label>
-                Username<input type="text" name="username" placeholder="username">
+                Username
             </label>
+            <input type="text" name="username" placeholder="username">
             <label>
-                Password<input type="password" name="password" placeholder="password">
+                Password
             </label>
+            <input type="password" name="password" placeholder="password">
             <button type="submit">Log In</button>
         </form>
         <p>Don't have an account? <a href="../pages/register.php">Sign up</a></p>
