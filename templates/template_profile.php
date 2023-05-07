3e83ad82e83aa233bde9efd94df61740fab3fcfa
Matilde Simões <up202108782@up.pt>
2023-05-07 16:50:49 +0100

    update profile.php

--- templates/template_profile.php
+++ templates/template_profile.php
@@ -8,17 +8,14 @@
         <a href="../pages/profile.php">Edit Profile</a>
         <a href="../pages/password.php">Change Password</a>
         <form action="../actions/action_edit_profile.php" method="post" class="profile">
-            <label for="firstname">First Name</label>
+            <label for="firstName">First Name</label>
             <input type="text" name="firstName" value="<?=$user->getFirstName()?>">
-            <label>
-                Last Name<input type="text" name="lastName" value="<?=$user->getLastName()?>">
-            </label>
-            <label>
-                Username<input type="text" name="username" value="<?=$user->getUsername()?>">
-            </label>
-            <label>
-                Email<input type="email" name="email" value="<?=$user->getEmail()?>">
-            </label>
+            <label for="lastName">Last Name</label>
+            <input type="text" name="lastName" value="<?=$user->getLastName()?>">
+            <label for="username">Username</label>
+            <input type="text" name="username" value="<?=$user->getUsername()?>">
+            <label for="email">Email</label>
+            <input type="email" name="email" value="<?=$user->getEmail()?>">
             <button type="submit">Save</button>
         </form>
     </section>
