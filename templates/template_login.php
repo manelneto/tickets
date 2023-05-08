290faaa5351891efae3eb5489c780fc9fff8ac9c
Joana Marques <joana893.marques@gmail.com>
2023-05-09 00:43:53 +0100

    minor update to login.php

--- templates/template_login.php
+++ templates/template_login.php
@@ -6,14 +6,14 @@
     <section id="login">
         <h2>Login</h2>
         <form action="../actions/action_login.php" method="post" class="login">
-            <label>
+            <label for="username">
                 Username
             </label>
-            <input type="text" name="username" placeholder="username">
-            <label>
+            <input id="username" type="text" name="username" placeholder="username">
+            <label for="password">
                 Password
             </label>
-            <input type="password" name="password" placeholder="password">
+            <input id="password" type="password" name="password" placeholder="password">
             <button type="submit">Log In</button>
         </form>
         <p>Don't have an account? <a href="../pages/register.php">Sign up</a></p>
