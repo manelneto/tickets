d613216b9f4a7bb256a263fd500e9235ec4275dc
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-11 13:09:23 +0100

    added required fields (do labels need id?)

--- templates/template_login.php
+++ templates/template_login.php
@@ -7,14 +7,10 @@
         <section id="login">
             <h2>Login</h2>
             <form action="../actions/action_login.php" method="post" class="login">
-                <label for="username">
-                    Username
-                </label>
-                <input id="username" type="text" name="username" placeholder="username">
-                <label for="password">
-                    Password
-                </label>
-                <input id="password" type="password" name="password" placeholder="password">
+                <label for="username">Username</label>
+                <input id="username" type="text" name="username" placeholder="username" required>
+                <label for="password">Password</label>
+                <input id="password" type="password" name="password" placeholder="password" required>
                 <button type="submit">Log In</button>
             </form>
             <p>Don't have an account? <a href="../pages/register.php">Sign up</a></p>
