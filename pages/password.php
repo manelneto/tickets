0e41402df46617794bdf50fd932494d5e1f4aa84
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-11 15:37:16 +0100

    merged profile and password

--- pages/password.php
+++ pages/password.php
@@ -16,9 +16,9 @@
     $user = User::getUser($db, $session->getId());
 
     require_once(__DIR__ . '/../templates/template_common.php');
-    require_once(__DIR__ . '/../templates/template_password.php');
+    require_once(__DIR__ . '/../templates/template_profile.php');
 
     drawHeader($session, 'My Profile');
-    drawPassword($user);
+    drawPassword();
     drawFooter();
 ?>
--- templates/template_profile.php
+++ templates/template_profile.php
@@ -1,22 +1,43 @@
 <?php
     declare(strict_types = 1);
+
+    require_once(__DIR__ . '/../database/class_user.php');
 ?>
 
-<?php function drawProfile(User $user) { ?>
+<?php function drawProfile(User $user) : void { ?>
     <main>
         <section id="profile">
             <h2>My Profile</h2>
-            <a class="profileTitle" href="../pages/profile.php">Edit Profile</a>
-            <a class="profilePassword" href="../pages/password.php">Change Password</a>
+            <a id="edit-profile" href="../pages/profile.php">Edit Profile</a>
+            <a id="change-password" href="../pages/password.php">Change Password</a>
             <form action="../actions/action_edit_profile.php" method="post" class="profile">
-                <label for="firstName">First Name</label>
-                <input type="text" name="firstName" value="<?=$user->getFirstName()?>">
-                <label for="lastName">Last Name</label>
-                <input type="text" name="lastName" value="<?=$user->getLastName()?>">
+                <label for="first">First Name</label>
+                <input id="first" type="text" name="first" value="<?=$user->getFirstName()?>" required>
+                <label for="last">Last Name</label>
+                <input id="last" type="text" name="last" value="<?=$user->getLastName()?>" required>
                 <label for="username">Username</label>
-                <input type="text" name="username" value="<?=$user->getUsername()?>">
+                <input id="username" type="text" name="username" value="<?=$user->getUsername()?>" required>
                 <label for="email">Email</label>
-                <input type="email" name="email" value="<?=$user->getEmail()?>">
+                <input id="email" type="email" name="email" value="<?=$user->getEmail()?>" required>
+                <button type="submit">Save</button>
+            </form>
+        </section>
+    </main>
+<?php } ?>
+
+<?php function drawPassword() : void { ?>
+    <main>
+        <section id="password">
+            <h2>Change Password</h2>
+            <a id="edit-profile" href="../pages/profile.php">Edit Profile</a>
+            <a id="change-password" href="../pages/password.php">Change Password</a>
+            <form action="../actions/action_edit_password.php" method="post" class="profile">
+                <label for="current">Current Password</label>
+                <input id="current" type="password" name="current" required>
+                <label for="new">New Password</label>
+                <input id="new" type="password" name="new" required>
+                <label for="confirm">Confirm Password</label>
+                <input id="confirm" type="password" name="confirm" required>
                 <button type="submit">Save</button>
             </form>
         </section>
