e763a36ad8913a0b85a18b84deb40515bfcf3dd9
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-11 12:34:00 +0100

    changed session: added messages and minor refactoring

--- utils/session.php
+++ utils/session.php
@@ -2,56 +2,61 @@
     declare(strict_types = 1);
 
     class Session {
+        private array $messages;
+
         public function __construct() {
             session_start();
+
+            $this->messages = $_SESSION['messages'] ?? array();
+            unset($_SESSION['messages']);
         }
 
         public function isLoggedIn() : bool {
             return isset($_SESSION['id']);
         }
 
-        public function logout() {
+        public function logout() : void {
             session_destroy();
         }
 
         public function getId() : ?int {
-            return $_SESSION['id'];
-        }
-
-        public function getUsername() : ?string {
-            return $_SESSION['username'];
+            return $_SESSION['id'] ?? null;
         }
 
         public function getName() : ?string {
-            return $_SESSION['name'];
+            return $_SESSION['name'] ?? null;
         }
 
         public function isAgent() : ?bool {
-            return $_SESSION['agent'];
+            return $_SESSION['agent'] ?? null;
         }
 
         public function isAdmin() : ?bool {
-            return $_SESSION['admin'];
+            return $_SESSION['admin'] ?? null;
         }
 
-        public function setId(int $id) {
+        public function setId(int $id) : void {
             $_SESSION['id'] = $id;
         }
 
-        public function setUsername(string $username) {
-            $_SESSION['username'] = $username;
-        }
-
-        public function setName(string $name) {
+        public function setName(string $name) : void {
             $_SESSION['name'] = $name;
         }
 
-        public function setAgent(bool $agent) {
+        public function setAgent(bool $agent) : void {
             $_SESSION['agent'] = $agent;
         }
 
-        public function setAdmin(bool $agent) {
-            $_SESSION['admin'] = $agent;
+        public function setAdmin(bool $admin) : void {
+            $_SESSION['admin'] = $admin;
+        }
+
+        public function getMessages() : array {
+            return $this->messages;
+        }
+
+        public function addMessage(string $type, string $text) : void {
+            $_SESSION['messages'][] = array('type' => $type, 'text' => $text);
         }
     }
 ?>
