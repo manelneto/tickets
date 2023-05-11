5a5c9f6ca6ecf1d4a607042de021911aa32a3440
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-11 15:41:10 +0100

    added required register fields

--- templates/template_register.php
+++ templates/template_register.php
@@ -2,30 +2,20 @@
     declare(strict_types = 1);
 ?>
 
-<?php function drawRegister() { ?>
+<?php function drawRegister() : void { ?>
     <section id="register">
         <h2>Register</h2>
         <form action="../actions/action_register.php" method="post" class="register">
-            <label for="firstname">
-                First Name
-            </label>
-            <input id="firstname" type="text" name="firstName" placeholder="first">
-            <label for="lastname">
-                Last Name
-            </label>
-            <input id="lastname" type="text" name="lastName" placeholder="last">
-            <label for="username">
-                Username
-            </label>
-            <input id="username" type="text" name="username" placeholder="username"> 
-            <label for="email">
-                Email
-            </label>
-            <input id="email" type="email" name="email" placeholder="email">
-            <label for="pass">
-                Password
-            </label>
-            <input id="pass" type="password" name="password" placeholder="password">
+            <label for="first">First Name</label>
+            <input id="first" type="text" name="first" placeholder="First Name" required>
+            <label for="last">Last Name</label>
+            <input id="last" type="text" name="last" placeholder="Last Name" required>
+            <label for="username">Username</label>
+            <input id="username" type="text" name="username" placeholder="Username" required>
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" placeholder="Email" required>
+            <label for="password">Password</label>
+            <input id="pass" type="password" name="password" placeholder="Password" required>
             <button type="submit">Register</button>
         </form>
         <p>Already have an account? <a href="../pages/login.php">Log in</a></p>
