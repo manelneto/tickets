a7d956ed2cff5f022e2c084470c34a024c77c816
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-11 15:12:50 +0100

    added required fields (perhaps select list should start empty?)

--- templates/template_management.php
+++ templates/template_management.php
@@ -20,9 +20,9 @@
                     </select>
                     <div id="upgrade-role">
                         <label for="agent">To agent</label>
-                        <input id="agent" type="radio" name="role" value="agent">
+                        <input id="agent" type="radio" name="role" value="agent" required>
                         <label for="admin">To admin</label>
-                        <input id=admin" type="radio" name="role" value="admin">
+                        <input id=admin" type="radio" name="role" value="admin" required>
                     </div>
                     <button type="submit">Upgrade</button>
                 </form>
@@ -31,7 +31,7 @@
                 <summary class="action">Add a new entity</summary>
                 <form action="../actions/action_add_entity.php" method="post" class="entity">
                     <label for="entity">Select an entity</label>
-                    <select id="entity" name="entity">
+                    <select id="entity" name="entity" required>
                         <option value="department">Department</option>
                         <option value="status">Status</option>
                         <option value="priority">Priority</option>
@@ -39,7 +39,7 @@
                     </select>
                     <div class="field"><!--não gosto desta div mas desisti de tentar-->
                         <label for="name">Name</label>
-                        <input type="text" id="name" name="name">
+                        <input type="text" id="name" name="name" required>
                     </div>
                     <button type="submit">Add</button>
                 </form>
@@ -48,14 +48,14 @@
                 <summary class="action">Assign agent to department</summary>
                 <form action="../actions/action_assign_agent.php" method="post" class="assign">
                     <label for="department">Select a department</label>
-                    <select id="department" name="department">
+                    <select id="department" name="department" required>
                         <?php foreach ($departments as $department) { ?>
                         <option value="<?=$department->getId()?>"><?=$department->getName()?></option>
                         <?php } ?>
                     </select>
                     <div class="field"><!--não gosto desta div mas desisti de tentar-->
                         <label for="agent">Select an agent</label>
-                        <select id="agent" name="agent">
+                        <select id="agent" name="agent" required>
                             <?php foreach ($agents as $agent) { ?>
                             <option value="<?=$agent->getId()?>"><?=$agent->getUsername()?></option>
                             <?php } ?>
