5aa5d9d16c07cc7b928dfc832a9784be5b4aa5ba
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-11 12:49:08 +0100

    minor changes and checks: classes and ids

--- templates/template_dashboard.php
+++ templates/template_dashboard.php
@@ -2,18 +2,18 @@
     declare(strict_types = 1);
 ?>
 
-<?php function drawDashboard(int $opened, int $assigned, int $closed) { ?>
-    <main class="dashboard-tickets">
-        <section class="opened">
-            <h2> Opened </h2>
+<?php function drawDashboard(int $opened, int $assigned, int $closed) : void { ?>
+    <main id="dashboard">
+        <section class="card">
+            <h2>Opened</h2>
             <p>You have <?=$opened?> tickets opened!</p>
         </section>
-        <section class="assigned">
+        <section class="card">
             <h2>Assigned</h2>
             <p>You have <?=$assigned?> tickets assigned!</p>
         </section>
-        <section class="closed">
-            <h2> Closed </h2>
+        <section class="card">
+            <h2>Closed</h2>
             <p>You have <?=$closed?> tickets closed!</p>
         </section>
     </main>
