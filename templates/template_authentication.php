bbf409e6c57b5730f155d2bdd889f4030246337a
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-11 16:23:41 +0100

    nothing relevant

--- templates/template_authentication.php
+++ templates/template_authentication.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 ?>
 
 <?php function drawLogin() : void { ?>
-    <main>
+    <main id="authentication">
         <section id="login">
             <h2>Login</h2>
             <form action="../actions/action_login.php" method="post" class="login">
@@ -19,21 +19,23 @@ declare(strict_types = 1);
 <?php } ?>
 
 <?php function drawRegister() : void { ?>
-    <section id="register">
-        <h2>Register</h2>
-        <form action="../actions/action_register.php" method="post" class="register">
-            <label for="first">First Name</label>
-            <input id="first" type="text" name="first" placeholder="First Name" required>
-            <label for="last">Last Name</label>
-            <input id="last" type="text" name="last" placeholder="Last Name" required>
-            <label for="username">Username</label>
-            <input id="username" type="text" name="username" placeholder="Username" required>
-            <label for="email">Email</label>
-            <input id="email" type="email" name="email" placeholder="Email" required>
-            <label for="password">Password</label>
-            <input id="pass" type="password" name="password" placeholder="Password" required>
-            <button type="submit">Register</button>
-        </form>
-        <p>Already have an account? <a href="../pages/login.php">Log in</a></p>
-    </section>
+    <main id="authentication">
+        <section id="register">
+            <h2>Register</h2>
+            <form action="../actions/action_register.php" method="post" class="register">
+                <label for="first">First Name</label>
+                <input id="first" type="text" name="first" placeholder="First Name" required>
+                <label for="last">Last Name</label>
+                <input id="last" type="text" name="last" placeholder="Last Name" required>
+                <label for="username">Username</label>
+                <input id="username" type="text" name="username" placeholder="Username" required>
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" placeholder="Email" required>
+                <label for="password">Password</label>
+                <input id="pass" type="password" name="password" placeholder="Password" required>
+                <button type="submit">Register</button>
+            </form>
+            <p>Already have an account? <a href="../pages/login.php">Log in</a></p>
+        </section>
+    </main>
 <?php } ?>
--- templates/template_faqs.php
+++ templates/template_faqs.php
@@ -19,34 +19,34 @@
 <?php } ?>
 
 <?php function drawFAQsAgent(array $faqs) : void { ?>
-<main>
-    <section id="faqs">
-        <h2>FAQ</h2>
-        <form action="../actions/action_add_faq.php" method="post" class="add-faq">
-            <details class="faq">
-                <summary class="question">Add a new FAQ</summary>
-                <label id="add-question" for="question">Question</label>
-                <input id="question" type="text" name="question" placeholder="question" required>
-                <label id="add-answer" for="answer">Answer</label>
-                <textarea id="answer" class="answer" name="answer" placeholder="answer" required></textarea>
-                <button type="submit">Add</button>
-            </details>
-        </form>
-        <?php foreach($faqs as $faq) { ?>
-        <form method="post" class="faq-questions-agent">
-            <input type="hidden" name="id" value="<?=$faq->getId()?>">
-            <details class="faq">
-                <summary class="question">
-                    <input type="text" name="question" value="<?=$faq->getQuestion()?>" required>
-                </summary>
-                <textarea class="answer" name="answer" required><?=$faq->getAnswer()?></textarea>
-                <div class="faq-buttons">
-                    <button formaction="../actions/action_edit_faq.php">Edit</button>
-                    <button formaction="../actions/action_delete_faq.php">Delete</button>
-                </div>
-            </details>
-        </form>
-        <?php } ?>
-    </section>
-</main>
+    <main>
+        <section id="faqs">
+            <h2>FAQ</h2>
+            <form action="../actions/action_add_faq.php" method="post" class="add-faq">
+                <details class="faq">
+                    <summary class="question">Add a new FAQ</summary>
+                    <label id="add-question" for="question">Question</label>
+                    <input id="question" type="text" name="question" placeholder="question" required>
+                    <label id="add-answer" for="answer">Answer</label>
+                    <textarea id="answer" class="answer" name="answer" placeholder="answer" required></textarea>
+                    <button type="submit">Add</button>
+                </details>
+            </form>
+            <?php foreach($faqs as $faq) { ?>
+            <form method="post" class="faq-questions-agent">
+                <input type="hidden" name="id" value="<?=$faq->getId()?>">
+                <details class="faq">
+                    <summary class="question">
+                        <input type="text" name="question" value="<?=$faq->getQuestion()?>" required>
+                    </summary>
+                    <textarea class="answer" name="answer" required><?=$faq->getAnswer()?></textarea>
+                    <div class="faq-buttons">
+                        <button formaction="../actions/action_edit_faq.php">Edit</button>
+                        <button formaction="../actions/action_delete_faq.php">Delete</button>
+                    </div>
+                </details>
+            </form>
+            <?php } ?>
+        </section>
+    </main>
 <?php } ?>
