d61ea2be9249e5d8fd0ae60b234d197849743d99
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-13 16:47:30 +0100

    added login checks

--- actions/action_login.php
+++ actions/action_login.php
@@ -9,19 +9,30 @@
         die();
     }
 
+    $username = strtolower(trim($_POST['username']));
+    $password = $_POST['password'];
+
+    if ($username === '') {
+        $session->addMessage(false, 'Username cannot be empty');
+        header('Location: ../pages/login.php');
+        die();
+    }
+
     require_once(__DIR__ . '/../database/connection.php');
     $db = getDatabaseConnection();
 
     require_once(__DIR__ . '/../database/class_user.php');
-    $user = User::loginUser($db, strtolower(trim($_POST['username'])), $_POST['password']);
+    $user = User::loginUser($db, $username, $password);
 
     if ($user) {
         $session->setId($user->getId());
         $session->setName($user->getName());
         $session->setAgent($user->isAgent($db));
         $session->setAdmin($user->isAdmin($db));
+        $session->addMessage(true, 'Login successful');
         header('Location: ../pages/dashboard.php');
     } else {
         header('Location: ../pages/login.php');
+        $session->addMessage(false, 'Login unsuccessful');
     }
 ?>
