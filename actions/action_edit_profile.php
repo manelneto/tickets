3acece5a922bd40875a6f7f33cc0c3109608aaac
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-13 16:36:37 +0100

    added edit_profile checks

--- actions/action_edit_profile.php
+++ actions/action_edit_profile.php
@@ -9,14 +9,28 @@
         die();
     }
 
+    $firstName = trim($_POST['first-name']);
+    $lastName = trim($_POST['last-name']);
+    $username = strtolower(trim($_POST['username']));
+    $email = strtolower(trim($_POST['email']));
+
+    if ($firstName === '' || $lastName === '' || $username === '' || $email == '') {
+        $session->addMessage(false, 'Profile fields cannot be empty');
+        header('Location: ../pages/profile.php');
+        die();
+    }
+
     require_once(__DIR__ . '/../database/connection.php');
     $db = getDatabaseConnection();
 
     require_once(__DIR__ . '/../database/class_user.php');
     $user = User::getUser($db, $session->getId());
 
-    if ($user && $user->edit($db, trim($_POST['first-name']), trim($_POST['last-name']), strtolower(trim($_POST['username'])), strtolower(trim($_POST['email']))))
+    if ($user && $user->edit($db, $firstName, $lastName, $username, $email)) {
         $session->setName($user->getName());
+        $session->addMessage(true, 'Profile successfully edited');
+    } else
+        $session->addMessage(false, 'Username/Email already exists');
 
     header('Location: ../pages/profile.php');
 ?>
