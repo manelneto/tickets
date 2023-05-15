ddcdd2efef4425ae056cabdda7dc5f55d2d6a0ab
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-05-15 20:08:51 +0100

    XSS: session-set-cookie-params

--- utils/session.php
+++ utils/session.php
@@ -5,6 +5,7 @@
         private array $messages;
 
         public function __construct() {
+            session_set_cookie_params(0, '/', 'localhost', true, true);
             session_start();
 
             $this->messages = $_SESSION['messages'] ?? array();
