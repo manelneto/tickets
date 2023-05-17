5d04ececeaecef53368e70002cb3c52f8cb6b55e
Matilde Simões <up202108782@up.pt>
2023-05-17 14:21:49 +0100

    fix upload photo

--- actions/action_upload_photo.php
+++ actions/action_upload_photo.php
@@ -20,7 +20,7 @@
     $user = User::getUser($db, $session->getId());
     
     $save_dir = "../profile_photos/";
-    $original_name = basename($_FILES["file-upload"]["name"]);
+    $original_name = basename($_FILES["photo-upload"]["name"]);
     $photo_type = pathinfo($original_name, PATHINFO_EXTENSION);
 
     $save_file = $save_dir . $session->getId() . "." . $photo_type ;
@@ -34,8 +34,8 @@
         unlink($save_file);
     }
 
-    if (move_uploaded_file($_FILES["file-upload"]["tmp_name"], $save_file) && $user->updatePhoto($db, $photo_type))
-        $session->addMessage(false, 'Success uploading profile photo');
+    if (move_uploaded_file($_FILES["photo-upload"]["tmp_name"], $save_file) && $user->updatePhoto($db, $photo_type))
+        $session->addMessage(true, 'Success uploading profile photo');
     else
         $session->addMessage(false, 'Error uploading profile photo');
 
